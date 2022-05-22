d8351cd9de0d1b54575387df7e95f1b69028c29a
AmanKhys <amankhys2112@gmail.com>
2022-05-23 01:42:55 +0530

    working counter made by accessing counter component by importing the counter component from another file values passed to counter component using {count = count} and accessing it in counter component using props.count

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import {useState } from 'react'
+import Counter from './Counter'
 function App() {
   const [count,setCount] = useState(0)
   const addCount=()=>{
-    setCount(count+1)    
+    setCount(count+1)
+    
   }
   return (
     <div className="App">
       <button onClick={addCount}>Add</button>
-
-      <h1>Counter:{count} </h1>
+    <Counter count={count}/>
     </div>
   );
 }
